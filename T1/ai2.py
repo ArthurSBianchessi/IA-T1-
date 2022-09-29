f5bfb2827a1628345e06d44568d9526da9259447
Arthur Bianchessi <arthursbianchessi@gmail.com>
2022-09-29 16:04:44 -0300

    bunch of things 2

--- T1/ai2.py
+++ T1/ai2.py
@@ -6,126 +6,103 @@ import sys
 
 def main():
     num_agents = int(sys.argv[1])
-    if num_agents%2==0:
-        num_agents+=1
     grid, n = read_file()
+    grid = add_borders(grid, n)
     num_coins = int(n/2)
-    agent_length = 4*(count_movable_spaces(grid) + num_coins + 1)
+    movabel_spaces = count_movable_spaces(grid) + num_coins + 1
+    agent_length = 4*(movabel_spaces)
     agents = gen_agents(agent_length, num_agents)
-    
 
-    # for agent in agents:
-    #     move(agent, grid, n)
-    # evaluate(agents, n)
-    # agents = reproduce(agents, n)
-    # prt(agents[0])
-    # for agent in agents:
-    #     move(agent, grid, n)
-    # prt(agents[0])
     best = 0
-    print(n)
-    print(count_movable_spaces(grid)+ num_coins + 1)
-    print()
-    for i in range(100):
-        move(agents, grid, n, agent_length, num_coins)
-        agents = evaluate(agents, n)
-
-        # for agent in agents:
-        #     print(agent.points)
-        # print()
-
-        # print(agents[0].points)
-        # print(agents[1].points)
-        # print(agents[2].points)
-        # print()
-
-        agents = mutate(agents, agent_length)
+    print("{0:>5}".format("Gen"), "{0:>10}".format(
+        "Points"), "{0:>5}".format("Coins"))
+    gen = 1
+    best_gen = 0
+    best_agent = agents[0]
+    while True:
+        print("\r{0:>5}".format(gen), "{0:>10}".format(
+                best_agent.posx), "{0:>5}".format(best_agent.posy), end="")
+        move(agents, grid, agent_length, num_coins)
+        agents, best_agent = evaluate(agents, n, movabel_spaces)
+        if gen - best_gen < min(100, 0.5*best_gen):
+            agents = mutate(agents, 100)
+        else:
+            agents = mutate(agents, 20)
         agents = reproduce(agents)
-
 
         if best_agent.points > best:
             best = best_agent.points
-            print(best, " ", best_agent.coins, i)
+            print("\r{0:>5}".format(gen), "{0:>10}".format(
+                best), "{0:>5}".format(best_agent.coins))
+            best_gen = gen
+        if best_agent.coins == num_coins:
+            if gen > best_gen*1.1:
+                break
+        gen += 1
 
-        # prt(agents[0])
-    print("\n")
-    print(best_agent.posx, best_agent.posy)
-    print(best_agent.points)
-    # print(pd.DataFrame(grid))
-    print("\n")
-    grid2 = deepcopy(grid)
-    for p in best_agent.all_positions:
-        if grid2[p[0]][p[1]] == "C":
-            grid2[p[0]][p[1]] = 0
-        if grid2[p[0]][p[1]] == "0":
-            grid2[p[0]][p[1]] = 0
-        grid2[p[0]][p[1]] += 1
-    dt = pd.DataFrame(grid2)
-    dt = dt.replace(to_replace="1", value="\u2588")
-    print(dt)
+    best_agent = agent_path(best_agent)
+    print_best(grid, best_agent)
+    simplified_print(grid, best_agent)
 
-    print("\n")
-    for p in best_agent.all_positions:
-        if grid[p[0]][p[1]] == "C" or grid[p[0]][p[1]] == "2":
-            grid[p[0]][p[1]] = "2"
-        else:
-            grid[p[0]][p[1]] = "3"
-    extra_line=[]
-    data = grid
-    for i in range(n):
-        extra_line.append("\u2588")
-    data.append(extra_line.copy())
-    data.insert(0, extra_line.copy())
-    for line in data:
-        line.append("\u2588")
-        line.insert(0, "\u2588")
-    for line in grid:
-        for char in line:
-            if char == "2":
-                print("\033[96m\u2588\033[0m", end="")
-            elif char == "3":
-                print("\033[94m\u2588\033[0m", end="")
-            elif char == "1":
-                print("\u2588", end="")
-            elif char == "0":
-                print(" ", end="")
+def agent_path(agent):
+    agent.posx = 1
+    agent.posy = 1
+    for i in range(agent.num_moves):
+        agent.posx += agent.moves[i][0]
+        agent.posy += agent.moves[i][1]
+        agent.positions.append([agent.posy, agent.posx])
+    return agent
+
+
+
+def print_best(grid, best_agent):
+        print("\n")
+        grid2 = deepcopy(grid)
+        for p in best_agent.positions:
+            if grid2[p[0]][p[1]] == "C":
+                grid2[p[0]][p[1]] = 0
+            if grid2[p[0]][p[1]] == "0":
+                grid2[p[0]][p[1]] = 0
+            if isinstance(grid2[p[0]][p[1]], int):
+                grid2[p[0]][p[1]] += 1
+        dt = pd.DataFrame(grid2)
+        dt = dt.replace(to_replace="1", value="\u2588")
+        print(dt, end="\n\n\n")
+
+def simplified_print(grid, best_agent):
+        for p in best_agent.positions:
+            if grid[p[0]][p[1]] == "C" or grid[p[0]][p[1]] == "2":
+                grid[p[0]][p[1]] = "2"
             else:
-                print(char, end="")
-        print()
+                grid[p[0]][p[1]] = "3"
 
-
-def print_best(agent, grid):
-    agent.posy = 0
-    agent.posx = 0
-    for move in agent.moves:
-        agent.posx += move[0]
-        agent.posy += move[1]
-        # print(agent.posy, agent.posx)
-        print(move)
-        grid[agent.posy][agent.posx] = "\x1b[0;34;40m\u2588\x1b[0m"
-    df = pd.DataFrame(grid)
-    df = df.replace(to_replace="\u2588", value="\u25A0")
-    df = df.replace(to_replace="0", value=" ")
-    print(df)
-
-
-def prt(a):
-    print(a.posx, a.posy, a.coins, a.num_moves, a.points)
+        for line in grid:
+            for char in line:
+                if char == "2":
+                    print("\033[96m\u2588\033[0m", end="")
+                elif char == "3":
+                    print("\033[94m\u2588\033[0m", end="")
+                elif char == "1":
+                    print("\u2588", end="")
+                elif char == "0":
+                    print(" ", end="")
+                else:
+                    print(char, end="")
+            print()
 
 
 class Agent():
     def __init__(self, list):
         self.moves = list
-        self.posx = 0
-        self.posy = 0
+        self.posx = 1
+        self.posy = 1
         self.num_moves = 0
         self.coins = 0
         self.positions = []
-        self.all_positions = []
-        self.coin_positions = []
+        self.coins_positions = [(1,1)]
         self.points = 0
         self.distance = 0
-        self.distances = []
+        self.distances = 0
         self.repeats = 0
 
 
@@ -136,6 +113,18 @@ def read_file():
     for line in file:
         grid.append(line.split())
     return [grid, n]
+
+
+def add_borders(grid, n):
+    extra_line = []
+    for i in range(n):
+        extra_line.append("1")
+    grid.append(extra_line.copy())
+    grid.insert(0, extra_line.copy())
+    for line in grid:
+        line.append("1")
+        line.insert(0, "1")
+    return grid
 
 
 def gen_agents(length, num_agents):
@@ -153,62 +142,51 @@ def gen_agents(length, num_agents):
     return agent_list
 
 
-def move(agents, grid, n, agent_length, num_coins):
+def move(agents, grid, agent_length, num_coins):
     for agent in agents:
-        while True:
+        while agent.num_moves < agent_length and agent.coins < num_coins:
             agent.posx += agent.moves[agent.num_moves][0]
             agent.posy += agent.moves[agent.num_moves][1]
-            if 0 <= agent.posx < n and 0 <= agent.posy < n:
-                if grid[agent.posy][agent.posx] == "C" and not [agent.posy, agent.posx] in agent.coin_positions:
-                    agent.coins += 1
-                    agent.distances.append(agent.distance)
-                    agent.distance = 0
-                    agent.positions = []
-                    agent.points = 0
-                    agent.coin_positions.append([agent.posy, agent.posx])
-                elif grid[agent.posy][agent.posx] == "1":
-                    agent.posx -= agent.moves[agent.num_moves][0]
-                    agent.posy -= agent.moves[agent.num_moves][1]
-                    break
-            else:
+            if grid[agent.posy][agent.posx] == "C" and not (agent.posy, agent.posx) in agent.coins_positions:
+                agent.coins_positions.append((agent.posy, agent.posx))
+                agent.coins += 1
+                agent.distances += agent.distance
+                agent.distance = 0
+                agent.positions = []
+            elif grid[agent.posy][agent.posx] == "1":
                 agent.posx -= agent.moves[agent.num_moves][0]
                 agent.posy -= agent.moves[agent.num_moves][1]
                 break
 
-            agent.repeats += agent.positions.count([agent.posy, agent.posx])
-            agent.positions.append([agent.posy, agent.posx])
-            agent.all_positions.append([agent.posy, agent.posx])
+            # agent.repeats += agent.positions.count([agent.posy, agent.posx])
+            if (agent.posy, agent.posx) not in agent.positions:
+                agent.positions.append((agent.posy, agent.posx))
+                agent.distance += 1
 
-            if agent.num_moves >= agent_length - 1 or agent.coins == num_coins:
-                break
-            # if [agent.posy, agent.posx] in agent.positions:
-            #     continue
-            # else:
-            #     agent.positions.append([agent.posy, agent.posx])
             agent.num_moves += 1
-            agent.distance += 1
 
 
-def evaluate(agents, n):
+def evaluate(agents, n, movabel_spaces):
+    multiplier = 3*n*(movabel_spaces+2)
     for agent in agents:
-        agent.points = agent.coins*n*n + agent.distance - sum(agent.distances) - agent.repeats
+        agent.points = agent.coins*multiplier - agent.distances + n*agent.distance +abs(agent.posy - agent.coins_positions[-1][0])+abs(agent.posx - agent.coins_positions[-1][1])
+        # + n*len(agent.distances)#- agent.repeats
+
     agents.sort(key=lambda x: x.points, reverse=True)
-    return agents
+    return agents, agents[0]
 
 
 def reproduce(agents):
     new_agents = []
     new_agents.append(Agent(agents[0].moves))
-    global best_agent
-    best_agent = agents[0]
+    if not len(agents) % 2:
+        new_agents.append(Agent(agents[1].moves))
 
     for i in range(1, len(agents), 2):
-        # a1 = agents[max(random.randrange(0, len(agents)),
-        #          random.randrange(0, len(agents)))]
-        # a2 = agents[max(random.randrange(0, len(agents)),
-        #          random.randrange(0, len(agents)))]
-        a1 = champ(agents)
-        a2 = champ(agents)
+        a1 = agents[min(random.randrange(0, len(agents)),
+                        random.randrange(0, len(agents)))]
+        a2 = agents[min(random.randrange(0, len(agents)),
+                        random.randrange(0, len(agents)))]
 
         first_error = max(a1.num_moves, a2.num_moves)
         length = random.randrange(0, first_error+1)
@@ -218,25 +196,18 @@ def reproduce(agents):
             Agent(a2.moves[:length] + a1.moves[length:]))
     return new_agents
 
-def champ(agents):
-    a1 = agents[random.randrange(len(agents))]
-    a2 = agents[random.randrange(len(agents))]
-    if a1.points > a2.points:
-        return a1
-    else:
-        return a2
 
 def count_movable_spaces(grid):
     counter = 0
     for line in grid:
         for element in line:
             if element == "1":
-                counter+=1
+                counter += 1
     return counter
 
 
-def mutate(agents, agent_length):
-    max_mutation = int(len(agents)/20)
+def mutate(agents, mutation_param):
+    max_mutation = int(len(agents)/mutation_param)
     if max_mutation < 2:
         max_mutation = 2
     mutations = random.randrange(1, max_mutation)
@@ -245,14 +216,13 @@ def mutate(agents, agent_length):
         if agents[agent_pos].num_moves == 0:
             chromossome = 0
         else:
-            chromossome =   random.randrange(0, agents[agent_pos].num_moves+1)
+            chromossome = random.randrange(0, agents[agent_pos].num_moves+2)
         while True:
-                temp = [random.randrange(-1, 2, 1), random.randrange(-1, 2, 1)]
-                if temp != [0, 0]:
-                    break
+            temp = [random.randrange(-1, 2, 1), random.randrange(-1, 2, 1)]
+            if temp != [0, 0]:
+                break
         agents[agent_pos].moves[chromossome] = temp
     return agents
-
 
 
 
--- T1/test.py
+++ T1/test.py
@@ -1,19 +1,46 @@
-import pandas as pd
-file = open("labirinto1.txt")
-n = int(file.readline())
-data=[]
-for line in file:
-    data.append(line.split())
+# import pandas as pd
+# file = open("labirinto1.txt")
+# n = int(file.readline())
+# data=[]
+# for line in file:
+#     data.append(line.split())
 
-# print([1,2]+[2,1])
-# print(data[1])
-# print()
-# print(pd.DataFrame(data))
-df=pd.DataFrame(data)
-df = df.replace(to_replace="1", value="\u2588")
-df = df.replace(to_replace="0", value=" ")
-# print(df[0])
-# print(data[0])
+# # print([1,2]+[2,1])
+# # print(data[1])
+# # print()
+# # print(pd.DataFrame(data))
+# df=pd.DataFrame(data)
+# df = df.replace(to_replace="1", value="\u2588")
+# df = df.replace(to_replace="0", value=" ")
+# # print(df[0])
+# # print(data[0])
+# # for line in data:
+# #     for char in line:
+# #         if char == "1":
+# #             print("\u2588", end="")
+# #         elif char == "0":
+# #             print(" ", end="")
+# #         else:
+# #             print(char, end="")
+# #     print()
+
+# # print(df)
+
+# df = df.replace(to_replace="\u2588", value="\u25A0")
+# df = df.replace(to_replace="0", value=" ")
+# print(df)
+# # print("\u25A0")
+
+# extra_line=[]
+# for i in range(n):
+#     extra_line.append("\u2588")
+# data.append(extra_line.copy())
+# data.insert(0, extra_line.copy())
+# for line in data:
+#     line.append("\u2588")
+#     line.insert(0, "\u2588")
+
+
 # for line in data:
 #     for char in line:
 #         if char == "1":
@@ -24,33 +51,7 @@ df = df.replace(to_replace="0", value=" ")
 #             print(char, end="")
 #     print()
 
-# print(df)
 
-df = df.replace(to_replace="\u2588", value="\u25A0")
-df = df.replace(to_replace="0", value=" ")
-print(df)
-# print("\u25A0")
-
-extra_line=[]
-for i in range(n):
-    extra_line.append("\u2588")
-
-data.append(extra_line.copy())
-data.insert(0, extra_line.copy())
-for line in data:
-    line.append("\u2588")
-    line.insert(0, "\u2588")
-
-
-for line in data:
-    for char in line:
-        if char == "1":
-            print("\u2588", end="")
-        elif char == "0":
-            print(" ", end="")
-        else:
-            print(char, end="")
-    print()
-
-
-print("\033[94m\u2588\033[0m")
+# print("\033[94m\u2588\033[0m")
+print("\ta\tb\tc")
+print("\taa\tbbb\tc")
